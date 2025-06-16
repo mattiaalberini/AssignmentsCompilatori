74d92cd4513ccb5962eaeef4458c1427311c969c
Mattia <mattiaalberini22gmail.com>
2025-06-16 11:44:21 +0200

    Fusion

--- QuartoAssignment/LoopFusion.cpp
+++ QuartoAssignment/LoopFusion.cpp
@@ -4,6 +4,9 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/ScalarEvolution.h"
+#include "llvm/IR/Dominators.h"
+#include "llvm/Analysis/PostDominators.h"
+#include "llvm/Analysis/DependenceAnalysis.h"
 
 using namespace llvm;
 
@@ -11,6 +14,7 @@ namespace {
 
     struct LoopFusion: PassInfoMixin<LoopFusion> {
 
+        //Controllo se sono adiacenti
         bool isAdjacent(Loop *L0, Loop *L1) {
 
             if(L0->isGuarded() && L1->isGuarded()) {
@@ -42,18 +46,128 @@ namespace {
             return false; 
         }
 
+        //Controllo se iterano lo stesso numero di volte
         bool sameTripCount(Loop *L0, Loop *L1, ScalarEvolution &SE) {
             auto tripCountL0 = SE.getBackedgeTakenCount(L0);
             auto tripCountL1 = SE.getBackedgeTakenCount(L1);
+
+            //SE.getBackedgeTakenCount(L1) ritorna un SCEVCouldNotCompute se non è possibile prevedere il numero di backedge-taken
+            if(auto *testErrL0 = dyn_cast<SCEVCouldNotCompute>(tripCountL0)) return false;
+            if(auto *testErrL1 = dyn_cast<SCEVCouldNotCompute>(tripCountL1)) return false;
 
             if(tripCountL0 == tripCountL1) return true;
 
             return false;
         }
 
+        //Controllo se sono control flow equivalent
+        bool isControlFlowEquivalence(Loop *L0, Loop *L1, DominatorTree &DT, PostDominatorTree &PDT){
+            if(L0->isGuarded() && L1->isGuarded()) {
+                //Tutti e due guarded
+                if(DT.dominates(L0->getExitBlock()->getSingleSuccessor(), L1->getLoopGuardBranch()->getParent())) {
+                    outs() << "L0 domina L1\n";
+                    if(PDT.dominates(L1->getLoopGuardBranch()->getParent(), L0->getExitBlock()->getSingleSuccessor())) {
+                        outs() << "L1 postdomina L0\n";
+                        return true;
+                    }
+                }
+
+            } else {
+                //Tutti e due non guarded
+                if(DT.dominates(L0->getUniqueExitBlock(), L1->getLoopPreheader())) {
+                    outs() << "L0 domina L1\n";
+                    if(PDT.dominates(L1->getLoopPreheader(), L0->getUniqueExitBlock())) {
+                        outs() << "L1 postdomina L0\n";
+                        return true;
+                    }
+                }
+            } 
+
+            return false;
+        }
+
+        //Controllo se ci sono dipendenze negative
+        bool hasNotNegativeDependence(Loop *L0, Loop *L1, DependenceInfo &DI) {
+            
+            for(auto &BBL0 : L0->getBlocks()){
+                for(Instruction &IL0 : *BBL0) {
+                    if(auto *storeInstL0 = dyn_cast<StoreInst>(&IL0)) {
+                        //Nel primo loop c'è una store
+                        outs() << "Store inst L0\n";
+                        outs() << IL0 << "\n";
+
+                        for(auto &BBL1 : L1->getBlocks()){
+                            for(Instruction &IL1 : *BBL1) {
+                                if(auto *loadInstL1 = dyn_cast<LoadInst>(&IL1)) {
+                                    //Nel secondo loop c'è una load
+                                    outs() << "Load inst L1\n";
+                                    outs() << IL1 << "\n";
+                                    
+                                    if(auto Dep = DI.depends(&IL0, &IL1, true)) {
+                                        //Istruzioni con potenziale dipendenza
+                                        if(Dep) {
+                                            outs() << Dep->getLevels() <<"\n";
+                                            Dep->dump(outs());
+                                        } else {
+                                            outs() << "Nessuna dipendenza!.\n";
+                                        }                                                                     
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        bool loopFus(Loop *L0, Loop *L1) {
+            auto *IVL0 = L0->getCanonicalInductionVariable();
+            auto *IVL1 = L1->getCanonicalInductionVariable();
+
+            //Modifico gli usi della induction variable
+            IVL1->replaceAllUsesWith(IVL0);
+
+
+            //Modifiche CFG
+
+            auto HeaderL0 = L0->getHeader();
+            auto HeaderL1 = L1->getHeader();
+
+            auto ExitL1 = L1->getUniqueExitBlock();
+
+            //Non considero nel body l'header e il latch
+            auto BodyL0 = L0->getBlocks().drop_front(1).drop_back(1);
+            auto BodyL1 = L1->getBlocks().drop_front(1).drop_back(1);
+
+            auto LatchL0 = L0->getLoopLatch();
+            auto LatchL1 = L1->getLoopLatch();
+            
+            //Body L0 -> Body L1
+            BodyL0.back()->getTerminator()->setSuccessor(0, BodyL1.front());
+
+            //Body L1 -> Latch L0
+            BodyL1.back()->getTerminator()->setSuccessor(0, LatchL0);
+
+            //Header L1 -> Latch L1
+            BranchInst::Create(LatchL1, HeaderL1->getTerminator());
+            HeaderL1->getTerminator()->eraseFromParent();
+            
+            //Header L0 -> Exit L1
+            BranchInst::Create(BodyL0.front(), ExitL1, HeaderL0->back().getOperand(0), HeaderL0->getTerminator());
+            HeaderL0->getTerminator()->eraseFromParent();
+
+            return true;
+        }
+
+
         PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
             LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
             ScalarEvolution &SE = AM.getResult<ScalarEvolutionAnalysis>(F);
+            DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
+            PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
+            DependenceInfo &DI = AM.getResult<DependenceAnalysis>(F);
 
             int i = 0;
             Loop *L0, *L1;
@@ -68,6 +182,12 @@ namespace {
             if(isAdjacent(L1, L0)) outs() << "Adiacenti\n";
 
             if(sameTripCount(L1, L0, SE)) outs() << "Stesso trip count\n";
+
+            if(isControlFlowEquivalence(L1, L0, DT, PDT)) outs() << "Control flow equivalent\n";
+
+            if(hasNotNegativeDependence(L1, L0, DI)) outs() << "Nessuna dipendenza a distanza negativa\n";
+
+            if(loopFus(L1, L0)) outs() << "Loop fusion avvenuta correttamente\n";
         
             /*
             SmallVector<Loop *, 8> Worklist = LI.getLoopsInPreorder();
